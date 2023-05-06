72979bcb9a708a9475dd24cf85414ba4560b69b2
laurentyzhang <laurent.y.zhang@gmail.com>
2023-05-06 10:49:20 -0600

    Moved

--- v2/commutative/constant.go
+++
@@ -1,8 +0,0 @@
-package commutative
-
-const (
-	PATH    uint8 = 100
-	INT64   uint8 = 101
-	UINT64  uint8 = 102
-	UINT256 uint8 = 103
-)
--- v2/noncommutative/constant.go
+++
@@ -1,8 +0,0 @@
-package noncommutative
-
-const (
-	INT64  uint8 = 104
-	STRING uint8 = 105
-	BIGINT uint8 = 106
-	BYTES  uint8 = 107
-)
